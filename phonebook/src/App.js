3aef8e3ee57d6a3de23cc8fe6d97559fc82ec01e
djanyreason <djanyreason@gmail.com>
2023-07-06 19:12:31 -0400

    Displays names in phonebook sorted alphabetically
    This commit adds a new function to the App component which maintains
    a sorting of the phonebook entries alphabetically by name, assuming
    the list begins either empty or sorted. It also changes the check for
    repeated entries to leverage the alphaabetical sort so that it runs
    in O(log(n)) time, rather than O(n).
    
    Also - this commit addresses an oversight in the solution to exercise
    2.7 in the previous commit. The exercise asks for the comparison to
    reject duplicate names regardless of capitalization, but that was not
    implemented in the previous commit. This commit fixes that, both in
    the new comparison function and in the previous comparison (using
    reduce, now commented out but still present).

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -8,12 +8,49 @@ const App = () => {
 
   const handleNameEntry = (event) => {setNewName(event.target.value);}
 
+  //Function that adds an entry to phonebook 
+  //while maintaining alphabetical sort of names
+  const addPerson = (sortedPeople) => {
+
+    if(sortedPeople.length === 0) return [ { name: newName } ];
+
+    const mid = Math.ceil(sortedPeople.length / 2);
+
+    if(sortedPeople[mid-1].name.toLowerCase() < newName.toLowerCase()) {
+      return sortedPeople.slice(0,mid).concat(addPerson(sortedPeople.slice(mid)));
+    } else {
+      return addPerson(sortedPeople.slice(0,mid-1)).concat(sortedPeople.slice(mid-1));
+    }
+  };
+
+  const validateNameEntry = (begin, end) => {
+    if(begin === end) return false;
+
+    const mid = Math.floor((begin + end) / 2);
+
+    if(persons[mid].name.toLowerCase() === newName.toLowerCase()) return true;
+    else if (persons[mid].name.toLowerCase() < newName.toLowerCase()) {
+      return validateNameEntry(mid+1, end);
+    } else { return validateNameEntry(begin, mid); }
+  };
+
   const handleAddClick = (event) => {
     event.preventDefault();
-    if(persons.reduce((check, person) => (check || (person.name === newName)), false)) {
+
+    //I believe Full Stack Open wants us to use reduce, but I wrote a different
+    //function that leverages the alphabetical sort of person names to do the
+    //comparison in O(log(n)) instead of O(n); I left in code using reduce,
+    //commented out
+    
+    /*if(persons.reduce((check, person) => 
+                      (check || (person.name.toLowerCase() 
+                                 === newName.toLowerCase())), false)) */
+    if(validateNameEntry(0, persons.length))
+    {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName }));
+      //setPersons(persons.concat({ name: newName }));
+      setPersons(addPerson(persons));
       setNewName('');
     }
   }
