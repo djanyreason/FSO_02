cb6ffc005657daf15472740789d1775e0557fcba
djanyreason <djanyreason@gmail.com>
2023-07-07 10:24:17 -0400

    Completes exercises 2.9 and 2.10 adding a filter
    This commit adds a filter to the phonebook, which displays only
    entries with names that contain the string entered into the filter
    textbox. This is case insensitive. A seperate component is added for
    the filter, and saved in a separate Filter.js file.
    
    As the phonebook app is separated into multiple components in their
    own files, this completes exercise 2.10.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import Entry from './components/Entry';
 import PersonForm from './components/PersonForm';
+import Filter from './components/Filter';
 
 const App = () => {
   const [persons, setPersons] = useState([  ]); 
+  const [filter, setFilter] = useState('');
+
+  const updateFilter = (newFilter) => {
+    setFilter(newFilter);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter filterText={filter} updateFilter={updateFilter} />
+      <h2>add a new</h2>
       <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-        {persons.map(person => <Entry key={person.name} person={person} />)}
+      {persons.reduce((reduced, person) => (
+        (person.name.toLowerCase().search(filter.toLowerCase()) < 0) ?
+        reduced : reduced.concat(person))
+        ,[]).map(person => <Entry key={person.name} person={person} />)}
     </div>
   );
 };
